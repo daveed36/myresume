c031d031ab1c986c9ff312bf65cf053b0f819439
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-03-13 19:51:20 -0500

    firstcommit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/resume">Resume</a>
+                <a href="/progects">My Projects</a>
+                <a href="/aboutme">About me</a>
+            </div>
+        </nav>
+     )
+}
+ 
+export default Navbar;
